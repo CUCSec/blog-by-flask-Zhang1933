f60b22d295ef6cd9c6ac2ffb6d6f86b192b2bb57
Zhang1933 <193319076@qq.com>
2020-06-09 23:51:24 +0800

    修复首页请求图标时会出现报错。

--- app.py
+++ app.py
@@ -5,26 +5,25 @@ app=Flask(__name__)
 
 @app.route("/")
 def homePage():
-   
+    # print("###/")
     return render_template("home.html")
 
 @app.route("/about")
 def about():
-
     return render_template("about.html")
 
 
 @app.route("/articles_list")
 def article_list():
-  
+
     article_list=[str(x) for x in  (Path('./data').glob("**/*.txt"))]
     return render_template("articles_list.html",list=article_list)
 
 
-@app.route("/<path:url_path>")
+@app.route("/data/<url_path>")
 def article_display(url_path):
-   
-    with open(r"{}".format(url_path),"r",encoding="utf-8")as f:
+
+    with open(r"data/{}".format(url_path),"r",encoding="utf-8")as f:
         data=f.readlines()
     return render_template("articles.html",article=data)
 
