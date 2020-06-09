604db26d1208357eca57367c91816c6a85b2e195
Zhang1933 <193319076@qq.com>
2020-06-09 17:32:19 +0800

    首页链接把home改成了/，app.py中删去了一个没必要的app.route("home")分支

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ app=Flask(__name__)
 
 @app.route("/")
 def homePage():
-    return render_template("home.html")
-
-@app.route("/home")
-def home():
+   
     return render_template("home.html")
 
 @app.route("/about")
 def about():
+
     return render_template("about.html")
+
 
 @app.route("/articles_list")
 def article_list():
-
+  
     article_list=[str(x) for x in  (Path('./data').glob("**/*.txt"))]
     return render_template("articles_list.html",list=article_list)
 
+
 @app.route("/<path:url_path>")
 def article_display(url_path):
-    print(url_path)
+   
     with open(r"{}".format(url_path),"r",encoding="utf-8")as f:
         data=f.readlines()
     return render_template("articles.html",article=data)
