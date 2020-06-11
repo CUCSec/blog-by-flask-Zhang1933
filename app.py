661525b6a0e9e734f0daabfa4df4147fd9f5df92
Zhang1933 <193319076@qq.com>
2020-06-11 18:48:59 +0800

    加入显示上传时间的功能。 多加了一篇测试文本。

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from pathlib import Path
+import time
 
 app=Flask(__name__)
 
@@ -17,15 +18,20 @@ def about():
 def article_list():
 
     article_list=[str(x) for x in  (Path('./data').glob("**/*.txt"))]
-    return render_template("articles_list.html",list=article_list)
+    article_time=[ time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(Path(x).stat().st_ctime)) for x in article_list]
+    article_dict=zip(article_list,article_time)
+    
+    article_dict=sorted(article_dict,key=lambda x: x[1],reverse=True)
+    return render_template("articles_list.html",article_dict=article_dict)
 
 
 @app.route("/data/<url_path>")
 def article_display(url_path):
+    article_time=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(Path(r"data/{}".format(url_path)).stat().st_ctime))
 
     with open(r"data/{}".format(url_path),"r",encoding="utf-8")as f:
         data=f.readlines()
-    return render_template("articles.html",article=data)
+    return render_template("articles.html",article=data,the_time=article_time)
 
 
 if __name__ == "__main__":
